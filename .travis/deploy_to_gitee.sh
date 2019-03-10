66a674456a83b53b4ca3bd2388b003a711a2d842
Jeremy Xu <jeremyxu2010@gmail.com>
2019-03-10 21:29:20 +0800

    six commit

---
+++ .travis/deploy_to_gitee.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd public_gitee
+git config user.name "Jeremy Xu"
+git config user.email "jeremyxu2010@gmail.com"
+git init
+git add .
+msg="rebuilding site `date`"
+git commit -m "$msg"
+git remote add origin git@gitee.com:jeremy-xu/jeremy-xu.git
+git push -u --force origin master:master
